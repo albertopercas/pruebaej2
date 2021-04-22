c946466b6ffa7e556a656b627f3b652dbe44a149
Alberto <alberto@ejemplo.com>
2021-04-22 09:54:07 +0200

    primera subida

---
+++ MuestraFecha.java
@@ -0,0 +1,10 @@
+package ejercicios;
+
+import java.util.Date;
+
+public class MuestraFecha {
+    public static void main(String[] args) {
+        Date hoy=new Date();
+        System.out.println(hoy);
+    }   
+}
